40688ddef5d7b280dc74e3570b646e0f2b4d16c8
yungProg <gboakye007@outlook.com>
2025-03-04 17:35:20 +0000

    refactor

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,7 +1,12 @@
+game_dictionary = File.readlines('google-10000-english-no-swears.txt')
+
+def clean_game_dictionary(dic)
+  dic.map! { |vocab| vocab.strip.downcase }
+  dic.select! { |vocab| vocab.length > 4 && vocab.length < 13 }
+end
+
 def game
-  game_dictionary = File.readlines('google-10000-english-no-swears.txt')
-  game_dictionary.map! {|vocab| vocab.strip.downcase}
-  game_dictionary.select! { |vocab| vocab.length > 4 && vocab.length < 13 }
+  clean_game_dictionary(game_dictionary)
 
   word = game_dictionary.sample
   word_arr = word.split('')
@@ -15,14 +20,13 @@ def game
   puts 'Welcome to Hangman'
 
   puts '_ '*word.length
-  puts word
 
   loop do
     puts 'Enter guess'
     player_guess = gets.chomp.downcase
 
     frequency = word.count(player_guess)
-    incorrect_letters.push(player_guess) if frequency == 0
+    incorrect_letters.push(player_guess) unless frequency > 0 && incorrect_letters.include?(player_guess)
     puts "Incorrect guess remaining: #{incorrect_guess_remaining -= 1}" if frequency == 0
     puts incorrect_letters.inspect unless incorrect_letters.empty?
 
