1f45affb402b9c1ca29ca153722498492c5c427b
yungProg <gboakye007@outlook.com>
2025-03-07 18:21:08 +0000

    Save game

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,23 +1,38 @@
+require 'json'
+
 def clean_game_dictionary(dic)
   dic.map! { |vocab| vocab.strip.downcase }
   dic.select! { |vocab| vocab.length > 4 && vocab.length < 13 }
 end
 
 def take_user_input
+  alphabets = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
   loop do
     puts 'Enter guess'
     player_guess = gets.chomp!.downcase
-    if player_guess.length == 1
+    if player_guess.length == 1 && alphabets.include?(player_guess)
       return player_guess
     end
   end
 end
 
+def save_game(state)
+  File.open('save.txt', 'w') do |file|
+    file.puts(state.to_json)
+  end
+end
+
+def load_game
+  if File.exist?('save.txt')
+    state = JSON.parse(File.read('save.txt'))
+    p state
+  end
+end
+
 def game
   game_dictionary = File.readlines('google-10000-english-no-swears.txt')
-  clean_game_dictionary(game_dictionary)
 
-  word = dic.sample
+  word =   clean_game_dictionary(game_dictionary).sample
   word_arr = word.split('')
 
   incorrect_guess_remaining = 6
@@ -26,7 +41,6 @@ def game
 
   guess = []
   word.length.times { guess.push('_ ') }
-  puts 'Welcome to Hangman'
 
   puts '_ '*word.length
 
@@ -34,18 +48,31 @@ def game
     player_guess = take_user_input
 
     frequency = word.count(player_guess)
-    incorrect_letters.push(player_guess) if (frequency == 0 && !incorrect_letters.include?(player_guess))
-    puts "Incorrect guess remaining: #{incorrect_guess_remaining -= 1}" if frequency == 0
+    if (frequency == 0 && !incorrect_letters.include?(player_guess))
+      incorrect_letters.push(player_guess) 
+      incorrect_guess_remaining -= 1
+      puts "Incorrect guess remaining: #{incorrect_guess_remaining}"
+    end
     puts incorrect_letters.inspect unless incorrect_letters.empty?
-
-    frequency.times do |word_index|
+    
+    frequency.times do
       word_index = word_arr.index(player_guess)
-      guess[word_index] = player_guess + ' '
-      word_arr[word_index] = nil
+      if word_index
+        guess[word_index] = player_guess + ' '
+        word_arr[word_index] = nil
+      end
     end
 
     puts guess.join
     correct_guess = true if word == guess.join.gsub(' ', '')
+
+    # save_game([word, incorrect_guess_remaining, incorrect_letters, guess]) 
+    save_game({
+      :word => word,
+      :incorrect_guess_remaining => incorrect_guess_remaining,
+      :incorrect_letters => incorrect_letters,
+      :guess => guess
+    })
 
     if (incorrect_guess_remaining == 0 || correct_guess)
       puts word
@@ -53,6 +80,18 @@ def game
       break
     end
   end
+  replay()
 end
 
-game()
+def play
+  puts 'Welcome to Hangman'
+  game()
+end
+
+def replay
+  puts 'Do you want to play again? (Yes/No)'
+  play_another_round = gets.chomp.downcase
+  game() if play_another_round == 'yes'
+end
+
+load_game
